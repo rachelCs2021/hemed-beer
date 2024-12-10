267888030e54bdaaa56c71f65ef879c59e1eabdc
rachelCs2021 <rachelchasha@gmail.com>
2024-12-10 14:04:23 +0200

    initial files

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>./components/Home.vue
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/Home.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
